8c1e27275931d223edecaf4ee0a93c3b6ec650df
xiangsanliu <xiangjianjian@hotmail.com>
2019-04-26 22:42:29 +0800

    数据统计 done

--- src/components/nav/StatManage.vue
+++ src/components/nav/StatManage.vue
@@ -1,5 +1,12 @@
 <template>
-    <div id="StatManage"></div>
+    <div id="StatManage">
+        <div>书籍总播放量：{{stat.bookAmount}}</div>
+        <div>短音频总播放量：{{stat.shortAmount}}</div>
+        <div>书籍总点赞量：{{stat.bookThumb}}</div>
+        <div>短音频总点赞量：{{stat.shortThumb}}</div>
+        <div>书籍总评论量：{{stat.bookComment}}</div>
+        <div>短音频总评论量：{{stat.shortComment}}</div>
+    </div>
 </template>
 
 <script>
@@ -8,12 +15,19 @@
         name: "StatManage",
         data() {
             return {
-                stat: null
+                stat: {
+                    bookAmount: 0,
+                    shortAmount: 0,
+                    bookThumb: 0,
+                    shortThumb: 0,
+                    bookComment: 0,
+                    shortComment: 0
+                }
             }
         },
         created() {
             this.httpGet('/count', response => {
-                console.warn(response);
+                this.stat = response.content;
             })
         }
     }
